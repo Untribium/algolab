a202ee82994e9ea121f374f1f1c2264fe617201d
martipa <martipa@student.ethz.ch>
2016-10-01 23:07:08 +0200

    finished 24

--- 24-evolution_2.cpp
+++ 24-evolution_2.cpp
@@ -2,46 +2,89 @@
 #include<stdio.h>
 #include<unordered_map>
 #include<algorithm>
+#include<iostream>
 
 using namespace std;
 
-unordered_map<string, int> indices;
-vector<int> species;
-vector<int> ancestor;
-vector<char*> names;
+struct Species {
+    char* name;
+    int age;
+    int depth;
+    int left;
+    int right;
+    Species* p;
+    vector<Species*> c;
+};
+
+unordered_map<string, Species*> map;
+vector<Species> species;
+
+vector<vector<Species*>> intervals;
+
+int index;
+
+void dfs(Species* node, int d) {
+    node->depth = d;
+    node->left = index;
+    index++;
+
+    for(Species* c : node->c) {
+        dfs(c, d+1);
+    }
+
+    node->right = index;
+    index++;
+
+    if(intervals.size() < d+1) {
+        intervals.resize(d+1);
+    }
+    intervals[d].push_back(node);
+};
 
 void do_case() {
 
     int n, q;
     scanf("%i %i", &n, &q);
 
+    map.clear();
     species.clear();
-    species.assign(n+1, 0);
+    species.resize(n);
 
-    ancestor.clear();
-    ancestor.assign(n+1, 0);
-
-    names.clear();
-    names.resize(n+1);
-
-    for(int i = 1; i <= n; ++i) {
+    for(int i = 0; i < n; ++i) {
         char* name = new char[11];
         int age;
 
         scanf("%s %i", name, &age);
 
-        indices.insert({string(name), i});
-        names[i] = name;
-        species[i]= age;
+        species[i].name = name;
+        species[i].age = age;
+
+        map.insert({string(name), &species[i]});
     }
 
     for(int i = 0; i < n-1; ++i) {
         char n1[11], n2[11];
 
-        scanf("%s %s", &n1, &n2);
+        scanf("%s %s", n1, n2);
 
-        ancestor[indices[string(n1)]] = indices[string(n2)];
+        Species* parent = map[string(n2)];
+        Species* child = map[string(n1)];
+
+        child->p = parent;
+        parent->c.push_back(child);
     }
+
+    Species* root = &species[0];
+
+    while(root->p) {
+        root = root->p;
+    }
+
+    intervals.clear();
+
+    index = 0;
+
+    dfs(root, 0);
 
     for(int i = 0; i < q; ++i) {
         char name[11];
@@ -49,13 +92,41 @@ void do_case() {
 
         scanf("%s %i", &name, &age);
 
-        int last_i = indices[string(name)];
+        Species* target = map[string(name)];
 
-        while(ancestor[last_i] && species[ancestor[last_i]] <= age) {
-            last_i = ancestor[last_i];
+        int d_min = 0, d_max = target->depth;
+        int i_min, i_max;
+
+        while(true) {
+            int d_cur = (d_max+d_min)/2;
+
+            i_min = 0;
+            i_max = intervals[d_cur].size()-1;
+
+            while(i_min != i_max) {
+                int i_cur = (i_max+i_min)/2;
+
+                if(target->right <= intervals[d_cur][i_cur]->right) {
+                    i_max = i_cur;
+                }
+                else {
+                    i_min = i_cur+1;
+                }
+            }
+
+            if(d_min == d_max) {
+                break;
+            }
+
+            if(intervals[d_cur][i_min]->age > age) {
+                d_min = d_cur+1;
+            }
+            else {
+                d_max = d_cur;
+            }
         }
 
-        printf("%s ", names[last_i]);
+        printf("%s ", intervals[d_min][i_min]->name);
     }
 
     printf("\n");
