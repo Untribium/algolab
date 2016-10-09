c4d22557be592ce1875893a229bcc430ed07ac28
martipa <martipa@student.ethz.ch>
2016-10-09 19:25:18 +0200

    added 32

---
+++ 32-first_hit/32-first_hit.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+using namespace std;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+int main() {
+
+
+    return 0;
+}
