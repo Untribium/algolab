b800e98f31019296cd3e1784cc31d1c69809cf38
Patrice Marti <patrice.marti@eidechs.com>
2016-11-21 18:51:22 +0100

    added 21_2, faster

---
+++ 21-search_snippets_2.cpp
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+
+using namespace std;
+
+struct Occ {
+    int p; // position
+    int w; // word
+};
+
+bool operator<(const Occ& o1, const Occ& o2) {
+    return o1.p < o2.p;
+}
+
+int n;
+
+vector<Occ> os;
+vector<int> ms;
+
+void do_case() {
+    ms.clear();
+    os.clear();
+
+    scanf("%i", &n);
+
+    for(int i = 0; i < n; ++i) {
+        int m;
+        scanf("%i", &m);
+
+        ms.push_back(m);
+    }
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < ms[i]; ++j) {
+            Occ o;
+            o.w = i;
+            scanf("%i", &o.p);
+
+            os.push_back(o);
+        }
+    }
+
+    sort(os.begin(), os.end());
+
+    vector<int> cs(n, 0);
+    int c = 0, d = 0, min_d = INT_MAX;
+
+    int b = 0, e = 0; // begin, end
+
+    while(e < (int)os.size()) {
+        cs[os[e].w]++;
+
+        if(cs[os[e].w] == 1) {
+            c++;
+        }
+
+        d = os[e].p-os[b].p;
+
+        while(cs[os[b].w] > 1 && b < e) {
+            cs[os[b].w]--;
+            b++;
+            d = os[e].p-os[b].p;
+        }
+
+        if(c == n) {
+            min_d = min(min_d, d);
+        }
+
+        e++;
+    }
+
+    printf("%i\n", min_d+1);
+}
+
+int main() {
+    int t;
+    scanf("%i", &t);
+
+    while(t--) {
+        do_case();
+    }
+}
