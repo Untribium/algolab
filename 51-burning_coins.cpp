ca4bf3734220ea63e773abd09304b5b85b35332f
Patrice Marti <patrice.marti@eidechs.com>
2017-01-10 16:19:55 +0100

    changed 51

--- 51-burning_coins.cpp
+++ 51-burning_coins.cpp
@@ -13,16 +13,21 @@ void do_case(int t) {
     vector<int> vs(n, 0);
 
     // dp array
-    vector< vector< pair<int, int> > > dp(n, vector< pair<int, int> >(n, make_pair<int, int>(0, 0)));
+    vector< vector<int> > dp(n+1, vector< int >(n+1, 0));
 
     // read values
     for(int i = 0; i < n; ++i) {
         scanf("%i", &vs[i]);
+
+        dp[0][i] = -1000;
+        dp[i][0] = -1000;
     }
 
     // rounds, look at diagonals
-    for(int i = 0; i < n; ++i) {
+    for(int i = 1; i < n; ++i) {
+        for(int l = 0; l < n; ++l) {
 
+        }
     }
 }
 
