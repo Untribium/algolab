269c88671073d4380599c70f8a7ec3553ebdb619
martipa <martipa@student.ethz.ch>
2017-01-10 17:57:12 +0100

    finished 51

--- 51-burning_coins.cpp
+++ 51-burning_coins.cpp
@@ -1,34 +1,44 @@
 #include<stdio.h>
-#include<vector>
+#include<algorithm>
 
 using namespace std;
+
+int n;
+int vals[2500];
+int memo[2500][2500];
+
+int best(int l, int r) {
+
+    // base case
+    if(l+1 >= r) {
+        return max(vals[l], vals[r]);
+    }
+
+    // memo
+    if(!memo[l][r]) {
+        memo[l][r] = max(min(best(l+2, r), best(l+1, r-1))+vals[l], min(best(l+1, r-1), best(l, r-2))+vals[r]);
+    }
+
+    return memo[l][r];
+
+}
 
 void do_case(int t) {
 
     // #coins
-    int n;
     scanf("%i", &n);
-
-    // values
-    vector<int> vs(n, 0);
-
-    // dp array
-    vector< vector<int> > dp(n+1, vector< int >(n+1, 0));
 
     // read values
     for(int i = 0; i < n; ++i) {
-        scanf("%i", &vs[i]);
+        scanf("%i", &vals[i]);
 
-        dp[0][i] = -1000;
-        dp[i][0] = -1000;
-    }
-
-    // rounds, look at diagonals
-    for(int i = 1; i < n; ++i) {
-        for(int l = 0; l < n; ++l) {
-
+        // reset memo table
+        for(int j = 0; j < n; ++j) {
+            memo[i][j] = 0;
         }
     }
+
+    printf("%i\n", best(0, n-1));
 }
 
 int main() {
