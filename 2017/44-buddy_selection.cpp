60196812a37844e9c3cda953c779ca8dea0dd75b
martipa <martipa@student.ethz.ch>
2018-01-15 00:43:10 +0100

    44: added

---
+++ 2017/44-buddy_selection.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        
+    }
+
+    return 0;
+}
