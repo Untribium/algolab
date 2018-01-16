807a6d0a9d6aae3c9d1e83a3b7fe2d40892be2a8
martipa <martipa@student.ethz.ch>
2018-01-16 19:11:22 +0100

    53: finished

--- 2017/53-light_at_the_museum.cpp
+++ 2017/53-light_at_the_museum.cpp
@@ -42,12 +42,11 @@ int main() {
 
         for(int id = 0; id < (1 << S/2); ++id) {
 
-            vector<int> d(R);
+            vector<int> d(R, 0);
             int s = 0;
 
             for(uint ir = 0; ir < R; ++ir) {
 
-                d[ir] = 0;
                 s = 0;
 
                 for(uint is = 0; is < S/2; ++is) {
@@ -77,7 +76,7 @@ int main() {
 
                 for(uint is = 0; is < (S-S/2); ++is) {
                     if((id >> is) & 1) {
-                        d[ir] += e[ir][S/2+is];
+                        d[ir] -= e[ir][S/2+is];
                         s++;
                     }
                 }
@@ -93,56 +92,6 @@ int main() {
         } else {
             cout << "impossible" << endl;
         }
-
-        // vector<vector<int> > d2r(1 << (S-S/2), vector<int>(R));
-        //
-        // for(uint id = 0; id < d2.size(); ++id) {
-        //
-        //     d2[id].first = 0;
-        //     d2[id].second = &d2r[id];
-        //
-        //     for(uint ir = 0; ir < R; ++ir) {
-        //
-        //         d2r[id][ir] = b[ir];
-        //
-        //         for(uint is = 0; is < (S-S/2); ++is) {
-        //             if((id >> is) & 1) {
-        //                 d2r[id][ir] += e[ir][is];
-        //             }
-        //         }
-        //
-        //         d2r[id][ir] = t[ir] - d2r[id][ir];
-        //
-        //         d2[id].first += d2r[id][ir];
-        //     }
-        // }
-        //
-        // sort(d1.begin(), d1.end());
-        // sort(d2.begin(), d2.end());
-        //
-        // uint p1 = 0, p2 = 0, op2 = 0;
-        // int s = 100;
-        //
-        // while(p1 < d1.size()) {
-        //
-        //     if((p2 >= d2.size() || d1[p1].first < d2[p2].first) && op2) {
-        //         p1++;
-        //         p2 = op2;
-        //         op2 = 0;
-        //         continue;
-        //     }
-        //
-        //     if(d1[p1].first > d2[p2].first){
-        //         p2++; continue;
-        //     }
-        //
-        //     if(d1[p1].first == d2[p2].first) {
-        //         // check
-        //         p2++;
-        //     }
-        //
-        //
-        // }
     }
 
     return 0;
