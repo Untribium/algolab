509f416046a4485cd4eb8b86d90b0a1a2e926e47
martipa <martipa@student.ethz.ch>
2018-01-19 01:25:08 +0100

    6w: rewrite finished

--- 2017/6w-motorcycles/6w-motorcycles.cpp
+++ 2017/6w-motorcycles/6w-motorcycles.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<algorithm>
-#include<tuple>
 #include<vector>
 #include<limits.h>
 
@@ -10,32 +9,22 @@ typedef unsigned long long ulolo;
 
 struct rider {
     uint ax, bx, ay, by;
+    long long x, y;
     bool f;
     long long o;
     int i;
 };
 
-tuple<ulolo, ulolo, bool> mult(const rider &r1, const rider &r2) {
-    // cout << r1.ax << " " << r2.ay << " " << (r1.f ? "true" : "false") << " " << (r2.f ? "true" : "false") << endl;
-
-    ulolo q1, q2, g0, g1, g2;
-
-    g0 = r1.ay * r2.ax;
-    g1 = r1.ay * r2.bx;
-    g2 = r1.by * r2.ax;
-
-    q1 = g0 + (g1 << 32) + (g2 << 32);
-    q2 = r1.by * r2.bx;
-
-    return make_tuple(q1, q2, r1.f);
-}
-
 bool operator<(const rider &r1, const rider &r2) {
-    return mult(r1, r2) < mult(r2, r1);
+    __int128_t v1 = (__int128_t) r1.y * r2.x;
+    __int128_t v2 = (__int128_t) r1.x * r2.y;
+    return v1 == v2 ? r1.f < r2.f : v1 < v2;
 }
 
 bool operator!=(const rider &r1, const rider &r2) {
-    return mult(r1, r2) != mult(r2, r1);
+    __int128_t v1 = (__int128_t) r1.y * r2.x;
+    __int128_t v2 = (__int128_t) r1.x * r2.y;
+    return v1 != v2 || r1.f != r2.f;
 }
 
 int main() {
@@ -58,61 +47,40 @@ int main() {
             cin >> d[in].o >> x >> y;
 
             d[in].f = (y < d[in].o);
+
             y = d[in].f ? d[in].o - y : y - d[in].o;
 
-            d[in].ax = x & UINT_MAX;
-            d[in].bx = x >> 32;
-            d[in].ay = y & UINT_MAX;
-            d[in].by = y >> 32;
+            d[in].x = x;
+            d[in].y = y;
 
             d[in].i = in;
-
-            // d[in] = make_tuple(D2(x, (y < o ? o-y : y-o)), (y < o), o, in);
         }
 
         sort(d.begin(), d.end());
 
-        // for(auto ed : d) {
-        //     cout << ed.i << endl;
-        // }
-
         vector<int> r;
 
         rider l, c;
-        // D2 dl, dc; bool fl, fc; long long ol, oc; int il, ic;
 
         long long tho, ho = tho = LLONG_MIN, tlo, lo = tlo = LLONG_MAX;
 
         for(int in = 0; in < N; in++) {
 
-            // tie(dc, fc, oc, ic) = d[in];
             c = d[in];
 
-            if(l != c) {
+            if(in && l != c) {
                 ho = std::max(ho, tho);
                 lo = std::min(lo, tlo);
             }
-
-            // if(dc != dl || fc != fl) {
-            //     ho = std::max(ho, tho);
-            //     lo = std::min(lo, tlo);
-            // }
 
             if((c.f && c.o < lo) || (!c.f && c.o > ho)) {
                 r.push_back(c.i);
             }
 
-            // if(fc && oc < lo || !fc && oc > ho) {
-            //     r.push_back(ic);
-            // }
-
             tlo = std::min(tlo, c.o);
             tho = std::max(tho, c.o);
 
             l = c;
-
-            // dl = dc; fl = fc;
-
         }
 
         sort(r.begin(), r.end());
