b2bc85442e0f6cf20a28df91d9c90165a415bd1b
martipa <martipa@student.ethz.ch>
2018-01-20 18:06:51 +0100

    c0: added

---
+++ 2017/c0-newtiles.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--) {
+        
+    }
+
+    return 0;
+}
