f355827f5d997d498700d85b520c287d71c907d7
martipa <martipa@student.ethz.ch>
2018-01-23 02:31:07 +0100

    51: minor

--- 2017/51-burning_coins.cpp
+++ 2017/51-burning_coins.cpp
@@ -11,9 +11,7 @@ int take(int l, int r) {
     if(l > r) return 0;
     if(l == r) return v[l];
 
-    // max(min(xx--, x--x)+l, min(--xx, x--x)+r);
-
-    if(!m[l][r]) {
+    if(!m[l][r]) { // max(min(xx--, x--x)+l, min(--xx, x--x)+r);
         m[l][r] = max(v[l]+min(take(l+2, r), take(l+1, r-1)), v[r]+min(take(l+1, r-1), take(l, r-2)));
     }
 
@@ -35,20 +33,12 @@ int main() {
         v.clear();
         v.assign(N, 0);
 
-        for(auto em : m) {
-            em.clear();
-        }
+        for(auto em : m) em.clear();
         m.clear();
-
         m.assign(N, vector<int>(N, 0));
 
-
-        for(int &ev : v) {
-            cin >> ev;
-        }
+        for(int &ev : v) cin >> ev;
 
         cout << take(0, N-1) << endl;
-
     }
-
 }
