e439c673c85f11d5f823ef8a9f58b4d7476dd95e
martipa <martipa@student.ethz.ch>
2018-01-26 01:47:15 +0100

    dw:

--- 2017/dw-worldcup/dw-worldcup.cpp
+++ 2017/dw-worldcup/dw-worldcup.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
+#include<CGAL/QP_models.h>
+#include<CGAL/QP_functions.h>
+#include<CGAL/Gmpq.h>
+#include<CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 using namespace std;
+using namespace CGAL;
+
+typedef Gmpq                                            ET;
+typedef Quadratic_program<ET>                           QP;
+typedef Quadratic_program_solution<ET>                  QPS;
+
+typedef Exact_predicates_inexact_constructions_kernel   K;
+typedef Point_2<K>                                      P2;
+
+double ceil_to_double(const Quotient<ET> &e) {
+    double d = std::ceil(to_double(e));
+    while(d   <  e) d++;
+    while(d-1 >= e) d--;
+    return d;
+}
 
 int main() {
 
@@ -11,7 +30,119 @@ int main() {
     cin >> T;
 
     while(T--) {
-        
+
+        int W, S, C;
+        cin >> W >> S >> C;
+
+        vector<int> a(W);
+        vector<P2> pw(W), ps(S);
+
+        vector<vector<pair<int, int> > > ow(W);
+        vector<vector<pair<int, int> > > os(S);
+
+        vector<int> r(W*S);
+
+        QP lp(EQUAL, true, 0, false, 0);
+
+        for(int iw = 0; iw < W; ++iw) {
+            int xw, yw, sw;
+            cin >> xw >> yw >> sw >> a[iw];
+
+            pw[iw] = P2(xw, yw);
+
+            lp.set_b(iw, sw);                        // supply
+            lp.set_r(iw, SMALLER);
+        }
+
+        for(int is = 0; is < S; ++is) {
+            int xs, ys, d, u;
+            cin >> xs >> ys >> d >> u;
+
+            ps[is] = P2(xs, ys);
+
+            lp.set_b(W+is,   d);                     // demand
+            lp.set_r(W+is, EQUAL);
+            lp.set_b(W+S+is, 100*u);                 // alcohol
+            lp.set_r(W+S+is, SMALLER);
+        }
+
+        for(int iw = 0; iw < W; ++iw) {
+            for(int is = 0; is < S; ++is) {
+                int rev;
+                cin >> rev;
+
+                // lp.set_c(iw*S+is, -r*100);         // revenue
+                r[iw*S+is] = rev*100;
+
+                lp.set_a(iw*S+is, iw,     1);        // supply
+
+                lp.set_a(iw*S+is, W+is,   1);        // demand
+                lp.set_a(iw*S+is, W+S+is, a[iw]);    // alcohol
+            }
+        }
+
+        int co = 0;
+
+        for(int ic = 0; ic < C; ++ic) {
+            int xc, yc, rc;
+            cin >> xc >> yc >> rc;
+
+            P2 cc(xc, yc);
+
+            bool in = false;
+
+            for(int iw = 0; iw < W; ++iw) {
+                if(squared_distance(cc, pw[iw]) < rc) {
+                    in = true;
+                    ow[iw].emplace_back(-rc, co);
+                }
+            }
+
+            for(int is = 0; is < S; ++is) {
+                if(squared_distance(cc, ps[is]) < rc) {
+                    in = true;
+                    os[is].emplace_back(-rc, co);
+                }
+            }
+
+            if(in) {
+                co++;
+            }
+        }
+
+        for(int iw = 0; iw < W && C; ++iw) {
+            sort(ow[iw].begin(), ow[iw].end());
+        }
+
+        for(int is = 0; is < S && C; ++is) {
+            sort(os[is].begin(), os[is].end());
+        }
+
+        for(int iw = 0; iw < W; ++iw) {
+            for(int is = 0; is < S; ++is) {
+                int rev = r[iw*S+is];
+                if(C) {
+                    int ps = 0, pw = 0;
+
+                    while(ps < os[is].size() && pw < ow[iw].size() && os[is][ps] == ow[iw][pw]) {
+                        ps++; pw++;
+                    }
+
+                    while(ps++ < (int) os[is].size()) rev--;
+                    while(pw++ < (int) ow[iw].size()) rev--;
+                }
+
+                lp.set_c(iw*S+is, -rev);
+            }
+        }
+
+        QPS lps = solve_linear_program(lp, ET());
+
+        if(lps.is_infeasible()) {
+            cout << "RIOT!" << endl;
+        } else {
+            cout << (long long) -ceil_to_double(lps.objective_value()/100) << endl;
+        }
     }
 
     return 0;
